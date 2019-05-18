15bba56d9088ccb468157324cd478679946166b9
Loxiuras <petervgarderen@outlook.com>
2019-05-18 11:16:10 +0200

    Added Meta values

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Loxiuras">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 
