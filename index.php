ed8d303cd10608bfb67f3f2fd2463c20bb156cd9
unknown <petervgarderen@outlook.com>
2019-05-21 23:02:30 +0200

    Added God review bar

--- index.php
+++ index.php
@@ -8,13 +8,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./assets/stylesheets/fonts/montserrat.css">
     <link rel="stylesheet" type="text/css" href="./assets/stylesheets/minified/style.css">
+    <script defer src="https://pro.fontawesome.com/releases/v5.8.2/js/all.js" integrity="sha384-RLPiEwcAdrH2NjFcwJipJtlFoIN1xvqPYeeDX5yYtSNu+HTIkQCDvPQ9thsUnPUS" crossorigin="anonymous"></script>
 </head>
 <body>
 
+<?php
+$_SESSION['items']['A'] = "A";
+$_SESSION['items']['B'] = "B";
+?>
+
 <div id="site">
     <?php require_once "mvc/modules/header.php"; ?>
+
+    <?php require_once "mvc/modules/product-banner.php"; ?>
 </div>
 
-<script defer src="https://pro.fontawesome.com/releases/v5.8.2/js/all.js" integrity="sha384-RLPiEwcAdrH2NjFcwJipJtlFoIN1xvqPYeeDX5yYtSNu+HTIkQCDvPQ9thsUnPUS" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ mvc/modules/product-banner.php
@@ -0,0 +1,35 @@
+<div class="product-banner">
+    <div class="inner">
+
+        <div class="products">
+            <div class="product">
+                <img class="product__image" src="assets/images/gods/cuchulainn.png" alt="Cuchulainn">
+                <div class="product-info">
+                    <h4 class="product__title">Cuchulainn</h4>
+                </div>
+            </div>
+
+            <div class="product">
+                <img class="product__image" src="assets/images/gods/daji.png" alt="Daji">
+                <div class="product-info">
+                    <h4 class="product__title">Daji</h4>
+                </div>
+            </div>
+
+            <div class="product">
+                <img class="product__image" src="assets/images/gods/ganesha.png" alt="Ganesha">
+                <div class="product-info">
+                    <h4 class="product__title">Ganesha</h4>
+                </div>
+            </div>
+
+            <div class="product">
+                <img class="product__image" src="assets/images/gods/cernnunos.png" alt="Cernnunos">
+                <div class="product-info">
+                    <h4 class="product__title">Cernnunos</h4>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
